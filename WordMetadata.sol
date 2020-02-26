f86d92d1cd8098dd0c871eb500d217b0278ea047
bombard-kb <38729453+bombard-kb@users.noreply.github.com>
2020-02-26 13:30:53 -0600

    Initial draft for Word Metadata

---
+++ WordMetadata.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.0;
+
+contract SENTENCE 
+{
+    using SafeMath for uint256;
+    
+    function tokenMetadata public view returns(string metadata)
+    {
+        string[] properties;
+        string[] attributes;
+        
+        string metadata = '{';
+        
+        for (uint i = 0; i < properties.length; ++i)
+            metadata += properties[i] + ',';
+            
+        metadata += '"attributes":['
+        
+        for (uint i = 0; i < attributes.length; ++i)
+        {
+            if (i > 0) metadata += ',';
+            metadata += attributes[i];
+        }
+        
+        metadata += ']}';
+        
+        return metadata; 
+    }
+}
